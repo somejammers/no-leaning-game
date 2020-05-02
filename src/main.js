3085ff859e442fcff742beea95a592fb9719de8e
AItheHuman <ariiramanesh@gmail.com>
2020-05-02 15:08:31 -0700

    increased speed scaling

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ let keyA, keyD, keyW, keyS;
 let resistance_keyDOWN, resistance_keyUP, 
     resistance_keyLEFT, resistance_keyRIGHT;
 
-let global_speed = 1.1, global_speed_default = 1, global_speed_scaling = 0.095, global_speed_max = 2.3
+let global_speed = 1.1, global_speed_default = 1, global_speed_scaling = 0.011, global_speed_max = 2.3
 let stageLeftBound, stageRightBound, stageUpperBound, stageLowerBound;
 
 let faller_x_on_barrier, faller_y_on_barrier;
